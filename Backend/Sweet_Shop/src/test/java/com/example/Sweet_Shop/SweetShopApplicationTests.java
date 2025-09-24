0449b25c8a9c54f932ccc5e8ba4d5be3e2ca628c
Komal Rathore <komalrathore0521@gmail.com>
2025-09-24 23:22:47 +0530

    feat: Initialise Spring Boot Backend Structure and  React frontend

---
+++ Backend/Sweet_Shop/src/test/java/com/example/Sweet_Shop/SweetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sweet_Shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
